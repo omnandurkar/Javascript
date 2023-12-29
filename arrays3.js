cc41fa7bc056f609cee92e92edcace535e68a59b
omnandurkar <nandurkarom172@gmail.com>
2023-12-29 16:09:42 +0530

    practice

--- arrays3.js
+++ arrays3.js
@@ -1,6 +1,19 @@
 console.log('Method On Arrays')
 
-const Friends = ["OM","Sunny","Mruganka","Purush", "Naari" ]
+let Friends = ["OM","Sunny","Mruganka","Purush", "Naari" ]
+let count=Friends.length
+console.log(count)
+
+console.log("Array is -",Friends)
+console.log("Adding new elements to the friends")
+Friends.push("Star")
+console.log(Friends)
+
+
+console.log("Array is -",Friends)
+console.log("Deleting LAST element using pop")
+Friends.pop()
+console.log(Friends)
 
 
 
