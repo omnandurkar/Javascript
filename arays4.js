b1e38a9fceb0174532687cc880a1b85f16ebf31f
omnandurkar <nandurkarom172@gmail.com>
2023-12-29 16:28:21 +0530

    Array LEARNED

---
+++ arays4.js
@@ -0,0 +1,25 @@
+const Friends = [ 'OM', 'Sunny', 'Mruganka', 'Purush', 'Naari' ]
+
+console.log("Array is - " ,Friends)
+
+Friends.unshift("Star")
+
+console.log("Array is - " ,Friends)
+
+Friends.shift()
+
+console.log("Array is - " ,Friends) 
+
+const index = Friends.indexOf("Naari")
+console.log(index)
+
+Friends.sort()
+console.log(Friends)
+
+Friends.reverse()
+console.log(Friends)
+
+console.log(Friends.join())
+
+
+
