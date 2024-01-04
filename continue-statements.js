0ceb7a09f66fc5b3010fc4eadb69c91ca34b0434
omnandurkar <nandurkarom172@gmail.com>
2024-01-04 07:32:03 +0530

    Continue Statements

---
+++ continue-statements.js
@@ -0,0 +1,15 @@
+for (let i = 0; i<21; i++)
+{
+    if(i%2 === 0){
+        continue;
+    }
+    console.log(i)
+}
+
+for (let i = 0; i<21; i++)
+{
+    if(i%2 != 0){
+        continue;
+    }
+    console.log(i)
+}
