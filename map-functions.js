afece1d85f285ce4b835c9873df1080a455238bf
omnandurkar <nandurkarom172@gmail.com>
2024-01-09 08:37:39 +0530

    Map Functions

--- map-functions.js
+++ map-functions.js
@@ -18,4 +18,17 @@ const doubleNumber  = number.map((number , index) => {
 }
 );
 
+
+
 //  with the map functions
+
+// Key Differences
+// forEach:
+
+// Does not return a new array.
+// Typically used for side effects (e.g., printing or modifying elements in place).
+// Cannot be used to create a new array with modified data directly.
+// map:
+
+// Returns a new array with the results of applying the provided function to each element.
+// Preferred when you need to transform or modify the data in an array and create a new array with the modified values.
