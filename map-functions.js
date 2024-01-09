5db4fe7c4d7dc9653b1e6d040b2e7929f8b22cf5
omnandurkar <nandurkarom172@gmail.com>
2024-01-09 08:35:42 +0530

    Map Functions

--- map-functions.js
+++ map-functions.js
@@ -10,3 +10,10 @@ const squareNumbers = numbers.map((number , index) =>{
 console.log(squareNumbers);
 
 // map functions
+
+const number = [1,2,3,4,5];
+
+const doubleNumber  = number.map((number , index) => {
+    return number * 2;
+}
+);
